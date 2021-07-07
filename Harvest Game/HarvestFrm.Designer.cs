75cf1f0e54a9165cfb19a37df45fb82c2365df34
Shiana French <shianafrench@mmc.school.nz>
2021-07-07 12:05:13 +1200

    Add project files.

---
+++ Harvest Game/HarvestFrm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Harvest_Game
+{
+    partial class HarvestFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Garden = new System.Windows.Forms.Panel();
+            this.MoneyLbl = new System.Windows.Forms.Label();
+            this.Money = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TmrSprite = new System.Windows.Forms.Timer(this.components);
+            this.TreeTime1 = new System.Windows.Forms.Timer(this.components);
+            this.Garden.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Garden
+            // 
+            this.Garden.BackColor = System.Drawing.Color.ForestGreen;
+            this.Garden.Controls.Add(this.MoneyLbl);
+            this.Garden.Controls.Add(this.Money);
+            this.Garden.Controls.Add(this.label1);
+            this.Garden.Location = new System.Drawing.Point(-5, -5);
+            this.Garden.Margin = new System.Windows.Forms.Padding(0);
+            this.Garden.Name = "Garden";
+            this.Garden.Size = new System.Drawing.Size(690, 570);
+            this.Garden.TabIndex = 0;
+            this.Garden.Paint += new System.Windows.Forms.PaintEventHandler(this.Garden_Paint);
+            // 
+            // MoneyLbl
+            // 
+            this.MoneyLbl.AutoSize = true;
+            this.MoneyLbl.Location = new System.Drawing.Point(588, 148);
+            this.MoneyLbl.Name = "MoneyLbl";
+            this.MoneyLbl.Size = new System.Drawing.Size(0, 15);
+            this.MoneyLbl.TabIndex = 2;
+            // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Location = new System.Drawing.Point(588, 111);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(44, 15);
+            this.Money.TabIndex = 1;
+            this.Money.Text = "Money";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // TmrSprite
+            // 
+            this.TmrSprite.Enabled = true;
+            this.TmrSprite.Interval = 10;
+            this.TmrSprite.Tick += new System.EventHandler(this.TmrSprite_Tick);
+            // 
+            // HarvestFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.Garden);
+            this.KeyPreview = true;
+            this.Name = "HarvestFrm";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.HarvestFrm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.HarvestFrm_KeyUp);
+            this.Garden.ResumeLayout(false);
+            this.Garden.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Garden;
+        private System.Windows.Forms.Timer TmrSprite;
+        private System.Windows.Forms.Timer TreeTime1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MoneyLbl;
+        private System.Windows.Forms.Label MoneyL;
+        private System.Windows.Forms.Label Money;
+    }
+}
+
---
+++ Harvest Game/HarvestFrm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Harvest_Game
+{
+    public partial class HarvestFrm : Form
+    {
+        Rectangle HotbarRec = new Rectangle(0, 0, 500, 80);
+        Image Hotbar = Properties.Resources.Hotbar;
+        Rectangle SpriteRec = new Rectangle(0, 0, 60, 60);
+        Image Sprite = Properties.Resources.Sprite;
+        Image Back = Properties.Resources.Sprite_Back;
+        Rectangle Tree1 = new Rectangle(20, 50, 100, 100);
+        Rectangle Tree2 = new Rectangle(120, 50, 100, 100);
+        Rectangle Tree3 = new Rectangle(220, 50, 100, 100);
+        Rectangle Tree4 = new Rectangle(390, 50, 100, 100);
+        Rectangle Tree5 = new Rectangle(490, 50, 100, 100);
+        Rectangle Tree6 = new Rectangle(590, 50, 100, 100);
+        Rectangle Hotbar1 = new Rectangle(0, 0, 500, 80);
+        Rectangle Hotbar2 = new Rectangle(0, 0, 500, 80);
+        Rectangle Hotbar3 = new Rectangle(0, 0, 500, 80);
+        Rectangle Hotbar4 = new Rectangle(0, 0, 500, 80);
+        Rectangle Hotbar5 = new Rectangle(0, 0, 500, 80);
+        Image Tree = Properties.Resources.Tree;
+        Image Little_Tree = Properties.Resources.Little_Tree;
+        int money = 500;
+
+
+
+        Graphics g;
+        bool left, right, up, down, peach1, peach2, nashi1, nashi2, apple1, apple2, front, back, one, two, three, four, five, interact;
+
+
+        private void HarvestFrm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false; }
+            if (e.KeyData == Keys.Right) { right = false; }
+            if (e.KeyData == Keys.Up) { up = false; }
+            if (e.KeyData == Keys.Down) { down = false; }
+            if (e.KeyData == Keys.A) { left = false; }
+            if (e.KeyData == Keys.D) { right = false; }
+            if (e.KeyData == Keys.W) { up = false; }
+            if (e.KeyData == Keys.S) { down = false; }
+            if (e.KeyData == Keys.Enter){interact = false;}
+
+        } 
+
+        private void HarvestFrm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) 
+            {
+                left = true; 
+            }
+            if (e.KeyData == Keys.Right) 
+            { 
+                right = true; 
+            }
+            if (e.KeyData == Keys.Up) 
+            { 
+                up = true;
+                back = true;
+                front = false;
+            }
+            if (e.KeyData == Keys.Down) 
+            { 
+                down = true;
+                front = true;
+                back = false;
+            }
+            if (e.KeyData == Keys.A)
+            {
+                left = true;
+            }
+            if (e.KeyData == Keys.D)
+            {
+                right = true;
+            }
+            if (e.KeyData == Keys.W)
+            {
+                up = true;
+                front = false;
+                back = true;
+            }
+            if (e.KeyData == Keys.S)
+            {
+                down = true;
+                front = true;
+                back = false;
+            }
+            if (e.KeyData == Keys.Enter) { interact = true; }
+        }
+
+        private void TmrSprite_Tick(object sender, EventArgs e)
+        {
+            if (left == true)
+            { 
+                SpriteRec.X -= 2; 
+            }//move sprite 2 to the left
+            if (right == true) 
+            { 
+                SpriteRec.X += 2; 
+            }// move sprite 2 to the right
+            if (down == true)
+            {
+                SpriteRec.Y += 2;
+            }
+            if (up == true)
+            {
+                SpriteRec.Y -= 2;
+            }
+            Garden.Invalidate();
+        }
+
+        public HarvestFrm()
+        {
+            InitializeComponent();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty 
+                | BindingFlags.Instance | BindingFlags.NonPublic, null, Garden, 
+                new object[] { true });
+        }
+
+        private void Garden_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            HotbarRec.X = 100;
+            HotbarRec.Y = 480;
+            if (TreeTime1.Enabled == false)
+            {
+                g.DrawImage(Little_Tree, Tree1);
+
+                if (interact == true)
+                {
+                    if (SpriteRec.IntersectsWith(Tree1))
+                    {
+                        money = money - 5;
+                        g.DrawImage(Tree, Tree1);
+                        TreeTime1.Enabled = true;
+
+                    }
+                }
+            }
+            g.DrawImage(Little_Tree, Tree1);
+            g.DrawImage(Little_Tree, Tree2);
+            g.DrawImage(Little_Tree, Tree3);
+            g.DrawImage(Little_Tree, Tree4);
+            g.DrawImage(Little_Tree, Tree5);
+            g.DrawImage(Little_Tree, Tree6);
+
+            if (front == true)
+            {
+                g.DrawImage(Sprite, SpriteRec);
+
+            }
+            if (back == true)
+            {
+                g.DrawImage(Back, SpriteRec);
+
+            }
+
+
+            g.DrawImage(Hotbar, HotbarRec);
+        }
+    }
+}
